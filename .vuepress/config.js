bd037300d0e6571eae0b33931e4a62e5d7fa8a55
qiphon <liqf@irongbei.com>
2019-08-19 16:21:43 +0800

    config change

---
+++ .vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: '齐枫的博客',
+    descript: '我的学习笔记',
+    base: '/',
+    themeConfig: {
+        nav: [
+            {
+                text: "首页",
+                link: "/"
+            },
+            {
+                text: "关于我",
+                link: "/about/aboutme"
+            },
+            {
+                text: "github",
+                link: "https://github.com/qiphon/yd-learn"
+            },
+        ]
+    },
+    // 侧边栏
+    sidebar: [
+        
+    ]
+}
