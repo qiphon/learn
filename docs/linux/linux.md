738bb673e5cda4560f8dbf354605efea267b5656
qiphon <13223394160@sina.cn>
2019-12-29 18:29:52 +0800

    add language

--- docs/linux/linux.md
+++ docs/linux/linux.md
@@ -18,3 +18,56 @@
 |---\  linux-start  按下开机键，Linux 做了什么？
 
 ```
+
+## 操作系统概述
+
+* 讲讲操作系统
+
+    * 古老的操作系统
+    * 更合适工作和娱乐的 windows
+    * 适合开发的linux  （服务器标准 POSIX）
+    * 非常好的 macOS  
+
+- linux 推荐 
+    - 服务器 centOS ubuntu
+    - 桌面版 fedora ubuntu
+
+centOS 和 fedora 都是 redhat 的发行版
+
+### 远程登陆 linux 
+
+- window 下
+    - putty
+    - Xshell
+    - Cmder 环境下使用 ssh 命令
+
+- linux 和 mac
+
+    - ssh 命令
+
+### 常用linux 命令
+
+- 文件编辑  vi / vim
+- 服务管理命令 systemctl  ( start stop status)
+- 网络管理命令 
+    - ifconfig(古老的命令) 、ip（现在使用）、
+    - route（网络路由信息）、
+    - ss -anp(查看打开的端口)、netstat -an ( | grep  筛选 )
+    - 停用服务 kill [pid] 、 systermctl(推荐使用)  一般主进程的pid 会小
+    - 下载命令 curl (很强大) 、 wget [url]( 方便 )
+    - 查看linux 命令的帮助  man [ls] 、ls --help
+    - 终端下 ctrl + s（锁住终端）后怎么办
+        - ctrl + c 结束正在运行的程序
+        - ctrl + d 结束输入或推出shell
+        - ctrl + s 暂停屏幕输出
+        - ctrl + q 恢复屏幕输出
+        - ctrl + l 清屏
+        - ctrl + a / ctrl + e 光标移动到行首或行尾
+
+### linux 进程管理相关命令
+
+- top 命令 查看内存占用
+- ps aux  查看进程  （主要看 user pid command(系统的路径和启动参数，放在[]里面的一般是系统内核级进程，不要动)，pid 等于1 的进程是所有进程的基础进程）
+- kill（kill 命令后面跟着的是 进程 pid）/pkill（pkill 后面跟着的是 服务名）  结束命令
+- w (查看当前登陆的用户)，last (查看所有用户的登陆历史，-n 展示条数) 、lastb (登陆失败记录)
+
--- docs/练习题/问答答案/4.bind.js
+++ docs/练习题/问答答案/4.bind.js
@@ -1,1 +1,40 @@
-见MDN
+Function.prototype.bind = function (othis) {
+    if (typeof this !== 'function') {
+        throw new TypeError('请使用函数执行')
+    }
+    var slice = Array.prototype.slice
+    var aArgs = slice.call(arguments, 1),
+        // 当前的函数
+        fToBind = this,
+        fNop = function () { },
+        fBound = function (args) {
+            return fToBind.apply(this instanceof fBound ? this : othis, aArgs.concat(slice.call(arguments)))
+        }
+    if (fToBind.prototype) {
+        fNop.prototype = fToBind.prototype
+    }
+    fBound.prototype = new fNop();
+    return fBound
+}
+
+if (!Function.prototype.bind) (function () {
+    var slice = Array.prototype.slice
+    Function.prototype.bind = function () {
+        var fToBind = this,
+            othis = arguments[0],
+            aArgs = slice.call(arguments, 1);
+        if (typeof fToBind !== 'function') {
+            throw new TypeError('Function.prototype.bind - ' +
+                'what is trying to be bound is not callable');
+        }
+        var fBound = function () {
+            var funcArgs = aArgs.concat(slice.call(arguments))
+            return fToBind.apply(othis, funcArgs)
+        }
+        if (fToBind.prototype) {
+            fNop.prototype = fToBind.prototype
+        }
+        fBound.prototype = new fNop();
+        return fBound
+    }
+})();
