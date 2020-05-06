cfddd95ed5e9f04c9ca1824256f17266f8da15c2
qiphon <767521025@qq.com>
2020-05-06 18:30:31 +0800

    add shell

---
+++ docs/练习题/算法/6.js
@@ -0,0 +1,15 @@
+/**
+ * 字符串排列
+ * 输入一个字符串，按字典顺序打印出该字符串中字符的所有排列。
+ * 例如输入字符串 abc， 则打印出 a,b,c所能排列出的所有字符串 
+ * abc, acb, bac, bca, cab, cba.
+ */
+
+const getGroup = (str)=>{
+    let len = str && str.length
+    if(!len) return;
+    if(len === 1) return str;
+    else{
+
+    }
+}
