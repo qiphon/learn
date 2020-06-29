1560e4254cd2b201f367c7fcabd862596eccfac8
deepin20_qifeng <767521025@qq.com>
2020-06-30 01:19:46 +0800

    添加 vuex

--- docs/js精粹/vuex/vue-demo/src/store/createStore.js
+++ docs/js精粹/vuex/vue-demo/src/store/createStore.js
@@ -1,15 +1,70 @@
 let Vue;
 
-class Vuex {
-    constructor(){
-
+class Store {
+    constructor(options){
+        // console.log(options, 'Store options')
+        const {
+            mutations,
+            actions,
+            modules,
+            getters,
+            state
+        } = options;
+        // 保证数据实现双向绑定
+        this._vm = new Vue({
+            data: {
+                $$state: state
+            }
+        })
+        this._mutations = mutations
+        this._actions = actions
+        const store = this
+        const { dispatch, commit } = store
+        this.commit = function bindcommit(type, payload, options){
+            commit.call(store, type, payload, options)
+        }
+        this.dispatch = function binddispatch(type, payload){
+            dispatch.call(store, type, payload)
+        }
+        if(getters){
+            this.handlegetters(getters)
+        }
+    }
+    get state(){
+        // console.log(this._vm)
+        return this._vm._data.$$state
+    }
+    dispatch(type, payload){
+        const entry = this._actions[type]
+        entry && entry(this, payload)
+    }
+    commit(type, payload, options){
+        const entry = this._mutations[type]
+        entry && entry(this.state, payload, options)
+    }
+    handlegetters(getters){
+        this.getters = {}
+        Object.keys(getters).forEach(getter =>{
+            // console.log(getter, 'getters', this)
+            Object.defineProperty(this.getters, getter, {
+                get: () => getters[getter](this.state)
+            })
+        })
     }
 }
 
 function install (_Vue){
     Vue = _Vue;
+    Vue.mixin({
+        beforeCreate(){
+            if(this.$options.store){
+                Vue.prototype.$store = this.$options.store
+            }
+        }
+    })
 }
 
-export {
-    Vuex,
+export default {
+    Store,
+    install
 }
--- docs/js精粹/vuex/vue-demo/src/store/index.js
+++ docs/js精粹/vuex/vue-demo/src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vuex from 'vuex'
+import Vuex from './createStore'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter: 0
   },
   mutations: {
+    add(state, payload){
+      console.log(payload, 'commit')
+      if(payload){
+        state.counter += payload
+      }else{
+        state.counter ++
+      }
+    }
   },
   actions: {
+    add({commit}, payload){
+      console.log(payload)
+      setTimeout(()=> commit('add', payload), 500)
+    }
   },
   modules: {
+  },
+  getters: {
+    doubleCounter(state){
+      return state.counter * 2
+    }
   }
 })
