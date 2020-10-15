11a4fbf2adc097b51a69e5b2efb8137084a14580
qiphon <qiphon@irongbei.com>
2020-10-15 18:32:19 +0800

    没写完，大数相乘

--- docs/数据结构&算法/算法/大数相乘.js
+++ docs/数据结构&算法/算法/大数相乘.js
@@ -26,6 +26,30 @@ num1 和 num2 均不以零开头，除非是数字 0 本身。
  * @param {string} num2
  * @return {string}
  */
+// var multiply = function(num1, num2) {
+//     if(num2 ==='0' || num1 ==='0') return '0'
+//     let up = 0
+//     let len1 = num1.length
+//     let len2 = num2.length
+//     let res = new Array(len1 + len2)
+//     for(let i=0; i< len1; i++){
+//         if(up > 0){
+//             res[len2 + i - 1] = up
+//         }
+//         up = 0
+//         for(let j = 0; j <len2; j++){
+//             if(i+j -len2 - len1-2 ===0 && up>0){
+//                 res = res[j + i + 1] = up
+//             }else {
+//                 let v = num1[len1 - i - 1] * num2[len2 - j - 1] 
+//                 let caculateV = v + (res[i + j] || 0) + up
+//                 res[i+j] = caculateV % 10
+//                 up = (caculateV - res[j+i]) / 10
+//             }
+//         }
+//     }
+//     return up> 0 ? up + res.reverse().join('') : res.reverse().join('')
+// };
 var multiply = function(num1, num2) {
     if(num2 ==='0' || num1 ==='0') return '0'
     let up = 0
