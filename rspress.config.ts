7ca5f83b4548bd44e33ee8bfb362ce0210a4c279
qifeng <qiphon3650@gmail.com>
2023-10-14 19:19:26 +0800

    配置更新

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'rspress/config'
-// import favoriteIcon from './docs/imgs/favorite.ico'
 
 export default defineConfig({
+  title: 'qiphon 博客',
+  description: '前端笔记,个人学习资料',
   // 文档根目录
   root: 'docs',
+  base: '/',
   route: {
     exclude: ['**/demos/**', '**/demo/**', '**/*.js', '**/*.ts', '**/*.tsx'],
   },
-  // icon: reqiure('./docs/imgs/favorite.ico'),
+  // favorite icon
+  icon: '/imgs/favorite.ico',
+  logo: '/imgs/favorite.ico',
 })
