442ef40e7cbc81866526b0fd29f789543fed4abf
qifeng <qiphon3650@gmail.com>
2023-10-28 23:31:20 +0800

    补充目录结构

---
+++ docs/aboutme/aboutme.tsx
@@ -0,0 +1,11 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <section>
+        <h3>职业：前端开发工程师</h3>
+      </section>
+    </div>
+  )
+}
+
+export default AboutMe
--- rspress.config.ts
+++ rspress.config.ts
@@ -12,4 +12,65 @@ export default defineConfig({
   // favorite icon
   icon: '/imgs/favorite.ico',
   logo: '/imgs/favorite.ico',
+  themeConfig: {
+    // nav: [
+    //   {
+    //     text: '我的',
+    //     items: [
+    //       // {
+    //       //   text: '其它资料',
+    //       //   items: [
+    //       //     {
+    //       //       text: 'Github',
+    //       //       link: 'http://github.com/qiphon',
+    //       //     },
+    //       //   ],
+    //       // },
+    //       {
+    //         text: '首页',
+    //         link: '/',
+    //       },
+    //       {
+    //         text: 'csdn',
+    //         link: 'https://qiphon.blog.csdn.net/',
+    //       },
+    //     ],
+    //     position: 'right',
+    //   },
+    // ],
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'http://github.com/qiphon',
+      },
+      {
+        icon: { svg: '<img src="/imgs/csdn.svg"/>' },
+        mode: 'link',
+        content: 'https://qiphon.blog.csdn.net/',
+      },
+    ],
+    // sidebar: {
+    //   '/guide/': [
+    //     {
+    //       text: 'Getting Started',
+    //       items: [
+    //         // 填入一个对象
+    //         {
+    //           text: 'Introduction',
+    //           link: '/guide/getting-started/introduction',
+    //         },
+    //         {
+    //           text: 'Installation',
+    //           link: '/guide/getting-started/installation',
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+    footer: { message: 'https://qiphon.blog.csdn.net/' },
+    lastUpdated: true,
+    outlineTitle: '本页主要内容',
+    outline: true,
+  },
 })
